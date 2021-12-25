72d921d545b660fae18ee59e7bd56a1e47dd3e10
giangtn <truongngocgiang99@gmail.com>
2021-12-26 00:41:19 +0700

    Update object name

--- src/ui/student/student.py
+++ src/ui/student/student.py
@@ -33,32 +33,32 @@ class Ui_StudentWidget(object):
         self.utilities_button.setHorizontalSpacing(1)
         self.utilities_button.setVerticalSpacing(3)
         self.utilities_button.setContentsMargins(0, 0, 0, 0)
-        self.view_attendance_history = QPushButton(self.gridLayoutWidget)
-        self.view_attendance_history.setObjectName(u"view_attendance_history")
+        self.view_attendance_history_button = QPushButton(self.gridLayoutWidget)
+        self.view_attendance_history_button.setObjectName(u"view_attendance_history_button")
         sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.view_attendance_history.sizePolicy().hasHeightForWidth())
-        self.view_attendance_history.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.view_attendance_history_button.sizePolicy().hasHeightForWidth())
+        self.view_attendance_history_button.setSizePolicy(sizePolicy)
 
-        self.utilities_button.addWidget(self.view_attendance_history, 0, 2, 1, 1)
+        self.utilities_button.addWidget(self.view_attendance_history_button, 0, 2, 1, 1)
 
-        self.update_face_id = QPushButton(self.gridLayoutWidget)
-        self.update_face_id.setObjectName(u"update_face_id")
+        self.update_face_id_button = QPushButton(self.gridLayoutWidget)
+        self.update_face_id_button.setObjectName(u"update_face_id_button")
         sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.update_face_id.sizePolicy().hasHeightForWidth())
-        self.update_face_id.setSizePolicy(sizePolicy1)
+        sizePolicy1.setHeightForWidth(self.update_face_id_button.sizePolicy().hasHeightForWidth())
+        self.update_face_id_button.setSizePolicy(sizePolicy1)
 
-        self.utilities_button.addWidget(self.update_face_id, 0, 0, 2, 1)
+        self.utilities_button.addWidget(self.update_face_id_button, 0, 0, 2, 1)
 
-        self.attendance = QPushButton(self.gridLayoutWidget)
-        self.attendance.setObjectName(u"attendance")
-        sizePolicy1.setHeightForWidth(self.attendance.sizePolicy().hasHeightForWidth())
-        self.attendance.setSizePolicy(sizePolicy1)
+        self.attendance_button = QPushButton(self.gridLayoutWidget)
+        self.attendance_button.setObjectName(u"attendance_button")
+        sizePolicy1.setHeightForWidth(self.attendance_button.sizePolicy().hasHeightForWidth())
+        self.attendance_button.setSizePolicy(sizePolicy1)
 
-        self.utilities_button.addWidget(self.attendance, 0, 1, 2, 1)
+        self.utilities_button.addWidget(self.attendance_button, 0, 1, 2, 1)
 
         self.user_info_table = QTableWidget(StudentWidget)
         if (self.user_info_table.columnCount() < 1):
@@ -100,9 +100,9 @@ class Ui_StudentWidget(object):
 
     def retranslateUi(self, StudentWidget):
         StudentWidget.setWindowTitle(QCoreApplication.translate("StudentWidget", u"Form", None))
-        self.view_attendance_history.setText(QCoreApplication.translate("StudentWidget", u"Xem l\u1ecbch s\u1eed \u0111i\u1ec3m danh", None))
-        self.update_face_id.setText(QCoreApplication.translate("StudentWidget", u"C\u1eadp nh\u1eadt khu\u00f4n m\u1eb7t", None))
-        self.attendance.setText(QCoreApplication.translate("StudentWidget", u"\u0110i\u1ec3m danh", None))
+        self.view_attendance_history_button.setText(QCoreApplication.translate("StudentWidget", u"Xem l\u1ecbch s\u1eed \u0111i\u1ec3m danh", None))
+        self.update_face_id_button.setText(QCoreApplication.translate("StudentWidget", u"C\u1eadp nh\u1eadt khu\u00f4n m\u1eb7t", None))
+        self.attendance_button.setText(QCoreApplication.translate("StudentWidget", u"\u0110i\u1ec3m danh", None))
         ___qtablewidgetitem = self.user_info_table.verticalHeaderItem(0)
         ___qtablewidgetitem.setText(QCoreApplication.translate("StudentWidget", u"T\u00ean sinh vi\u00ean:", None));
         ___qtablewidgetitem1 = self.user_info_table.verticalHeaderItem(1)
