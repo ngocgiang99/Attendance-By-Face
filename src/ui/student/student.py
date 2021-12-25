ffbf06e857beb8931a2b352625bfe0c78ceef66b
giangtn <truongngocgiang99@gmail.com>
2021-12-26 00:35:15 +0700

    Update object name

--- src/ui/student/student.py
+++ src/ui/student/student.py
@@ -16,8 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractItemView, QApplication, QGridLayout, QHeaderView, QLayout,
-    QPushButton, QSizePolicy, QTableWidget, QTableWidgetItem,
-    QTextBrowser, QWidget)
+    QLineEdit, QPushButton, QSizePolicy, QTableWidget,
+    QTableWidgetItem, QWidget)
 
 class Ui_StudentWidget(object):
     def setupUi(self, StudentWidget):
@@ -27,61 +27,71 @@ class Ui_StudentWidget(object):
         self.gridLayoutWidget = QWidget(StudentWidget)
         self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
         self.gridLayoutWidget.setGeometry(QRect(40, 580, 1321, 131))
-        self.gridLayout = QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.gridLayout.setSizeConstraint(QLayout.SetMaximumSize)
-        self.gridLayout.setHorizontalSpacing(1)
-        self.gridLayout.setVerticalSpacing(3)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.pushButton_2 = QPushButton(self.gridLayoutWidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.utilities_button = QGridLayout(self.gridLayoutWidget)
+        self.utilities_button.setObjectName(u"utilities_button")
+        self.utilities_button.setSizeConstraint(QLayout.SetMaximumSize)
+        self.utilities_button.setHorizontalSpacing(1)
+        self.utilities_button.setVerticalSpacing(3)
+        self.utilities_button.setContentsMargins(0, 0, 0, 0)
+        self.view_attendance_history = QPushButton(self.gridLayoutWidget)
+        self.view_attendance_history.setObjectName(u"view_attendance_history")
         sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
-        self.pushButton_2.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.view_attendance_history.sizePolicy().hasHeightForWidth())
+        self.view_attendance_history.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.pushButton_2, 0, 2, 1, 1)
+        self.utilities_button.addWidget(self.view_attendance_history, 0, 2, 1, 1)
 
-        self.pushButton = QPushButton(self.gridLayoutWidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.update_face_id = QPushButton(self.gridLayoutWidget)
+        self.update_face_id.setObjectName(u"update_face_id")
         sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy1)
+        sizePolicy1.setHeightForWidth(self.update_face_id.sizePolicy().hasHeightForWidth())
+        self.update_face_id.setSizePolicy(sizePolicy1)
 
-        self.gridLayout.addWidget(self.pushButton, 0, 0, 2, 1)
+        self.utilities_button.addWidget(self.update_face_id, 0, 0, 2, 1)
 
-        self.pushButton_3 = QPushButton(self.gridLayoutWidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        sizePolicy1.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
-        self.pushButton_3.setSizePolicy(sizePolicy1)
+        self.attendance = QPushButton(self.gridLayoutWidget)
+        self.attendance.setObjectName(u"attendance")
+        sizePolicy1.setHeightForWidth(self.attendance.sizePolicy().hasHeightForWidth())
+        self.attendance.setSizePolicy(sizePolicy1)
 
-        self.gridLayout.addWidget(self.pushButton_3, 0, 1, 2, 1)
+        self.utilities_button.addWidget(self.attendance, 0, 1, 2, 1)
 
-        self.textBrowser = QTextBrowser(StudentWidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(40, 90, 181, 21))
-        self.tableWidget = QTableWidget(StudentWidget)
-        if (self.tableWidget.columnCount() < 1):
-            self.tableWidget.setColumnCount(1)
+        self.user_info_table = QTableWidget(StudentWidget)
+        if (self.user_info_table.columnCount() < 1):
+            self.user_info_table.setColumnCount(1)
         __qtablewidgetitem = QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
-        if (self.tableWidget.rowCount() < 3):
-            self.tableWidget.setRowCount(3)
+        self.user_info_table.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        if (self.user_info_table.rowCount() < 3):
+            self.user_info_table.setRowCount(3)
         __qtablewidgetitem1 = QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(0, __qtablewidgetitem1)
+        self.user_info_table.setVerticalHeaderItem(0, __qtablewidgetitem1)
         __qtablewidgetitem2 = QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(1, __qtablewidgetitem2)
+        self.user_info_table.setVerticalHeaderItem(1, __qtablewidgetitem2)
         __qtablewidgetitem3 = QTableWidgetItem()
-        self.tableWidget.setVerticalHeaderItem(2, __qtablewidgetitem3)
+        self.user_info_table.setVerticalHeaderItem(2, __qtablewidgetitem3)
         __qtablewidgetitem4 = QTableWidgetItem()
-        self.tableWidget.setItem(0, 0, __qtablewidgetitem4)
-        self.tableWidget.setObjectName(u"tableWidget")
-        self.tableWidget.setGeometry(QRect(40, 140, 261, 111))
-        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.tableWidget.horizontalHeader().setDefaultSectionSize(160)
+        self.user_info_table.setItem(0, 0, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.user_info_table.setItem(1, 0, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.user_info_table.setItem(2, 0, __qtablewidgetitem6)
+        self.user_info_table.setObjectName(u"user_info_table")
+        self.user_info_table.setGeometry(QRect(40, 140, 351, 111))
+        self.user_info_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.user_info_table.horizontalHeader().setDefaultSectionSize(250)
+        self.user_info_table_tittle = QLineEdit(StudentWidget)
+        self.user_info_table_tittle.setObjectName(u"user_info_table_tittle")
+        self.user_info_table_tittle.setGeometry(QRect(40, 70, 191, 31))
+        font = QFont()
+        font.setFamilies([u"DejaVu Sans"])
+        font.setPointSize(12)
+        font.setBold(True)
+        self.user_info_table_tittle.setFont(font)
+        self.user_info_table_tittle.setReadOnly(True)
 
         self.retranslateUi(StudentWidget)
 
@@ -90,26 +100,26 @@ class Ui_StudentWidget(object):
 
     def retranslateUi(self, StudentWidget):
         StudentWidget.setWindowTitle(QCoreApplication.translate("StudentWidget", u"Form", None))
-        self.pushButton_2.setText(QCoreApplication.translate("StudentWidget", u"Xem l\u1ecbch s\u1eed \u0111i\u1ec3m danh", None))
-        self.pushButton.setText(QCoreApplication.translate("StudentWidget", u"C\u1eadp nh\u1eadt khu\u00f4n m\u1eb7t", None))
-        self.pushButton_3.setText(QCoreApplication.translate("StudentWidget", u"\u0110i\u1ec3m danh", None))
-        self.textBrowser.setHtml(QCoreApplication.translate("StudentWidget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:700; color:#ff5500;\">Th\u00f4ng Tin sinh vi\u00ean</span></p></body></html>", None))
-        ___qtablewidgetitem = self.tableWidget.verticalHeaderItem(0)
+        self.view_attendance_history.setText(QCoreApplication.translate("StudentWidget", u"Xem l\u1ecbch s\u1eed \u0111i\u1ec3m danh", None))
+        self.update_face_id.setText(QCoreApplication.translate("StudentWidget", u"C\u1eadp nh\u1eadt khu\u00f4n m\u1eb7t", None))
+        self.attendance.setText(QCoreApplication.translate("StudentWidget", u"\u0110i\u1ec3m danh", None))
+        ___qtablewidgetitem = self.user_info_table.verticalHeaderItem(0)
         ___qtablewidgetitem.setText(QCoreApplication.translate("StudentWidget", u"T\u00ean sinh vi\u00ean:", None));
-        ___qtablewidgetitem1 = self.tableWidget.verticalHeaderItem(1)
+        ___qtablewidgetitem1 = self.user_info_table.verticalHeaderItem(1)
         ___qtablewidgetitem1.setText(QCoreApplication.translate("StudentWidget", u"MSSV:", None));
-        ___qtablewidgetitem2 = self.tableWidget.verticalHeaderItem(2)
+        ___qtablewidgetitem2 = self.user_info_table.verticalHeaderItem(2)
         ___qtablewidgetitem2.setText(QCoreApplication.translate("StudentWidget", u"Email:", None));
 
-        __sortingEnabled = self.tableWidget.isSortingEnabled()
-        self.tableWidget.setSortingEnabled(False)
-        ___qtablewidgetitem3 = self.tableWidget.item(0, 0)
+        __sortingEnabled = self.user_info_table.isSortingEnabled()
+        self.user_info_table.setSortingEnabled(False)
+        ___qtablewidgetitem3 = self.user_info_table.item(0, 0)
         ___qtablewidgetitem3.setText(QCoreApplication.translate("StudentWidget", u"Tr\u01b0\u01a1ng Ng\u1ecdc Giang", None));
-        self.tableWidget.setSortingEnabled(__sortingEnabled)
+        ___qtablewidgetitem4 = self.user_info_table.item(1, 0)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("StudentWidget", u"20170067", None));
+        ___qtablewidgetitem5 = self.user_info_table.item(2, 0)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("StudentWidget", u"giangtn.170067@sis.hust.edu.vn", None));
+        self.user_info_table.setSortingEnabled(__sortingEnabled)
 
+        self.user_info_table_tittle.setText(QCoreApplication.translate("StudentWidget", u"Th\u00f4ng Tin sinh vi\u00ean", None))
     # retranslateUi
 
