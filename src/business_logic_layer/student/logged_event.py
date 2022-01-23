03ebbc5ae055063a8f5a9f6b96bf71997659eacd
truong ngoc giang <giangtn@aimesoft.com>
2022-01-23 16:13:01 +0700

    add table info

--- src/business_logic_layer/student/logged_event.py
+++ src/business_logic_layer/student/logged_event.py
@@ -2,6 +2,7 @@ from business_logic_layer.student.face_id_updater_event import FaceIdUpdaterWidg
 from ui.student.faceid_updater import Ui_FaceIdUpdater
 from ui.student.student import Ui_StudentWidget
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import QCoreApplication
 import random
 
 from ui.student.student import Ui_StudentWidget
@@ -26,6 +27,19 @@ class Ui_StudentWidget_Logged(Ui_StudentWidget):
         
 
     def setup_table_info(self):
+        student_name = self.student_info.get('name', "error")
+        mssv = self.student_info.get('mssv', 0)
+        email = self.student_info.get('email', 'error@')
+
+
+
+        self.user_info_table.setSortingEnabled(False)
+        ___qtablewidgetitem3 = self.user_info_table.item(0, 0)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("StudentWidget", student_name, None));
+        ___qtablewidgetitem4 = self.user_info_table.item(1, 0)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("StudentWidget", str(mssv), None));
+        ___qtablewidgetitem5 = self.user_info_table.item(2, 0)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("StudentWidget", email, None));
         pass
 
     def setup_button_click(self):
